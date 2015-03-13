335504685ca7f8b256e40093dc1477e397332e5c
simon-zhang36 <simon.zhang36@gmail.com>
2015-03-13 07:06:02 -0400

    Quiz2 update

--- Exploratory Data Analysis/quiz2.R
+++ Exploratory Data Analysis/quiz2.R
@@ -1,5 +1,22 @@
+# Q2
+library(nlme)
+library(lattice)
+xyplot(weight ~ Time | Diet, BodyWeight)
+
+# Q4
+library(datasets)
+library(lattice)
+data(airquality)
+p <- xyplot(Ozone ~ Wind | factor(Month),data=airquality)
+
+
 # Q7
 library(datasets)
 data(airquality)
 library(ggplot2)
-qplot(Wind,Ozone,data=airquality)
+airquality <- transform(airquality, Month = factor(Month))
+qplot(Wind,Ozone,data=airquality,facets = . ~ Month)
+
+# Q10
+qplot(Wind,Ozone,data=airquality,facets = . ~ Month)
+qplot(Wind,Ozone,data=airquality,facets = . ~ Month) + geom_smooth()
