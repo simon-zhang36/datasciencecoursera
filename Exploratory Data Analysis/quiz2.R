aff50dce7e94b92e1695c76fbff44416df5f9bfb
simon-zhang36 <simon.zhang36@gmail.com>
2015-05-19 18:42:30 -0400

    quiz 2

--- Exploratory Data Analysis/quiz2.R
+++ Exploratory Data Analysis/quiz2.R
@@ -1,7 +1,17 @@
+# Q1
+y <- c(1,2,3,4,5)
+x <- c(1,2,3,4,5)
+class(xyplot(y~x))
+
+"an object of class "trellis""
+
+
 # Q2
 library(nlme)
 library(lattice)
 xyplot(weight ~ Time | Diet, BodyWeight)
+
+"A set of 3 panels showing the relationship between weight and time for each diet."
 
 # Q4
 library(datasets)
@@ -16,6 +26,11 @@ data(airquality)
 library(ggplot2)
 airquality <- transform(airquality, Month = factor(Month))
 qplot(Wind,Ozone,data=airquality,facets = . ~ Month)
+
+# Q9
+library(ggplot2)
+g <- ggplot(movies, aes(votes, rating))
+print(g)
 
 # Q10
 qplot(Wind,Ozone,data=airquality,facets = . ~ Month)
